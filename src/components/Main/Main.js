2253e4edbb6a3a8f39d3d13a378f5890f143d2fe
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-10-26 16:56:14 +0100

    Feat: Display videos list

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,40 +1,60 @@
 import "./Main.scss";
-import { useState } from "react";
-import videosJson from "../../data/videos.json";
-import videosDetailsJson from "../../data/video-details.json";
+import { useEffect, useState } from "react";
+// import videosJson from "../../data/videos.json";
+// import videosDetailsJson from "../../data/video-details.json";
 import VideoDetails from "../VideoDetails/VideoDetails";
 import VideoPlayer from "../VideoPlayer/VideoPlayer";
 import NewComment from "../NewComment/NewComment";
 import VideoComments from "../VideoComments/VideoComments";
 import VideosList from "../VideosList/VideosList";
+import axios from "axios";
 
 const Main = () => {
-  const [activeVideo, setActiveVideo] = useState(videosDetailsJson[0]);
+  const baseUrl = "https://project-2-api.herokuapp.com/";
+  const apiKey = "969924e0-6223-4838-a69b-6e5590910a1d";
+  const [activeVideo, setActiveVideo] = useState(null);
 
-  const handleVideoClicked = (clickedVideoId) => {
-    const clickedVideo = videosDetailsJson.find(
-      (video) => video.id === clickedVideoId
-    );
-    setActiveVideo(clickedVideo);
-  };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const { data } = await axios.get(`${baseUrl}videos/?api_key=${apiKey}`);
+  //       console.log(data);
+  //       setVideosList(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
-  const filterActiveVideo = videosJson.filter(
-    (video) => video.id !== activeVideo.id
-  );
+  // const handleVideoClicked = (clickedVideoId) => {
+  //   const clickedVideo = videosDetailsJson.find(
+  //     (video) => video.id === clickedVideoId
+  //   );
+  //   setActiveVideo(clickedVideo);
+  // };
+
+  // const filterActiveVideo = videosJson.filter(
+  //   (video) => video.id !== activeVideo.id
+  // );
+
+  if (!activeVideo) {
+    return <>Loading, time for a quick coffee</>;
+  }
 
   return (
     <main className="main">
-      <VideoPlayer videosData={activeVideo} />
+      {/* <VideoPlayer videosData={activeVideo} /> */}
       <div className="main__container">
         <div className="main__details">
-          <VideoDetails videosDetails={activeVideo} />
-          <NewComment />
-          <VideoComments videosDetails={activeVideo} />
+          {/* <VideoDetails videosDetails={activeVideo} /> */}
+          {/* <NewComment /> */}
+          {/* <VideoComments videosDetails={activeVideo} /> */}
         </div>
         <div className="main__playlist">
           <VideosList
-            videosData={filterActiveVideo}
-            handleVideoClicked={handleVideoClicked}
+          // videosData={filterActiveVideo}
+          // handleVideoClicked={handleVideoClicked}
           />
         </div>
       </div>
--- src/components/VideosList/VideosList.js
+++ src/components/VideosList/VideosList.js
@@ -1,18 +1,40 @@
+import { useEffect, useState } from "react";
 import "./VideosList.scss";
+import axios from "axios";
 
-const VideosList = ({ videosData, handleVideoClicked }) => {
+const VideosList = () => {
+  const baseUrl = "https://project-2-api.herokuapp.com/";
+  const apiKey = "969924e0-6223-4838-a69b-6e5590910a1d";
+  const [videosList, setVideosList] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${baseUrl}videos/?api_key=${apiKey}`);
+        setVideosList(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!videosList) {
+    return <>Loading, keep waiting lol...</>;
+  }
+
   return (
     <section className="videos-list">
       <h3 className="videos-list__title">NEXT VIDEOS</h3>
       <div className="videos">
-        {videosData.map((video) => {
+        {videosList.map((video) => {
           return (
             <button
               className="videos__item"
               key={video.id}
-              onClick={() => {
-                handleVideoClicked(video.id);
-              }}
+              // onClick={() => {
+              //   handleVideoClicked(video.id);
+              // }}
             >
               <div className="videos__image-container">
                 <img
